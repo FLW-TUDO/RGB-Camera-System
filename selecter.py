de020ad616f7125676545de9c369f9f64dd517b6
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2021-02-23 13:15:12 +0100

    fixed selection process

--- selecter.py
+++ selecter.py
@@ -4,25 +4,25 @@ import os
 import numpy as np
 
 aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
-                                    #columns, rows
+#columns, rows
 board = cv2.aruco.CharucoBoard_create(4, 3, 1, .8, aruco_dict)
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.00001)
 
-a = 7   # columns
-b = 5   # rows
+a = 5   # columns
+b = 3   # rows
 objp = np.zeros((1, a*b, 3), np.float32)
-objp[0, :, :2] = np.mgrid[0:a,0:b].T.reshape(-1, 2)
+objp[0, :, :2] = np.mgrid[0:a, 0:b].T.reshape(-1, 2)
 
-def chessboard(gray, img):
+
+def chessboard(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
     ret, corners = cv2.findChessboardCorners(gray, (a, b), None)
-    print(ret)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
-        return
         corners2 = cv2.cornerSubPix(
             gray, corners, (11, 11), (-1, -1), criteria)
         # Draw and display the corners
@@ -32,47 +32,44 @@ def chessboard(gray, img):
 
         # check the image to display the correct chessboard corners
         if key == ord('r'):
-            os.remove(fname)
+            return False
+        return True
     else:
-        os.remove(fname)
+        return False
+
 
 def charuco(gray):
     decimator = 0
     corners, ids, rejectedPoints = cv2.aruco.detectMarkers(gray, aruco_dict)
 
     # print(corners)
-    #print(rejectedPoints)
+    # print(rejectedPoints)
 
-    if len(corners)>0:
+    if len(corners) > 0:
         # SUB PIXEL DETECTION
         for corner in corners:
             cv2.cornerSubPix(gray, corner,
-                                winSize = (3,3),
-                                zeroZone = (-1,-1),
-                                criteria = criteria)
+                             winSize=(3, 3),
+                             zeroZone=(-1, -1),
+                             criteria=criteria)
         frame_markers = cv2.aruco.drawDetectedMarkers(img.copy(), corners, ids)
         cv2.imshow('Image', frame_markers)
         cv2.waitKey(0)
-        res2 = cv2.aruco.interpolateCornersCharuco(corners,ids,gray,board)
-        if res2[1] is not None and res2[2] is not None and len(res2[1])>3 and decimator%1==0:
+        res2 = cv2.aruco.interpolateCornersCharuco(corners, ids, gray, board)
+        if res2[1] is not None and res2[2] is not None and len(res2[1]) > 3 and decimator % 1 == 0:
             print(res2)
+
 
 # simple script to delete not usable images
 if __name__ == "__main__":
-    images = glob('./images/chessboard/*.jpg')
+    images = glob('./images/*.jpg')
     # images = glob('./images/charuco/*.jpg')
 
     for fname in images:
         img = cv2.imread(fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # find chessboard corners
-        chessboard(gray, img)
+        res = chessboard(img)
 
         # find charuco corners
         # charuco(gray)
-
-
-    
-
-    
