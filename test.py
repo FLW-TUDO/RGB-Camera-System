546d5580b40a4d9af81185be5cdbf28c9c6658b1
athos <frederik.polachowski@tu-dortmund.de>
2021-03-30 14:56:57 +0200

    test script for determining the camera position based on the elevation and rotation

--- test.py
+++ test.py
@@ -1,15 +1,42 @@
+import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import json
+from scipy.spatial.transform import Rotation as R
+
+
+def rotate(vector, degrees=90, axis=np.array([1, 0, 0])):
+    angle = np.radians(degrees)
+    angel_vector = axis * angle
+    r = R.from_rotvec(angel_vector)
+    new_vector = r.apply(vector)
+    return new_vector
+
+
+def calculate_position(point, length, camera_angle_hor, camera_angle_ver):
+    vector = np.array([-1, 0, 0])
+    vector = rotate(vector, camera_angle_ver, axis=np.array([0, 1, 0]))
+    vector = rotate(vector, camera_angle_hor, axis=np.array([0, 0, 1]))
+    vector *= length
+
+    final_point = point + vector
+    print(final_point)
+    return final_point
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-with open('./images/chessboard/data.json') as f:
-    data = json.load(f)
-    x = [point[0] for point in data['1.png']]
-    y = [point[1] for point in data['1.png']]
-    z = [point[1] for point in data['1.png']]
-    ax.scatter(x, y, z)
+point = np.array([9089, -467, 0])
+camera_angle_hor = 90
+camera_angle_ver = 45
+length = 7136
+
+ax.scatter(*point)
+final_point = calculate_position(
+    point, length, camera_angle_hor, camera_angle_ver)
+
+ax.scatter(*final_point)
+ax.plot(*zip(final_point, point))
+
 
 plt.show()
