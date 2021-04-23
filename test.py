b75884dd4b2bda2f2419ab989825968eeb872b96
athos <frederik.polachowski@tu-dortmund.de>
2021-04-23 16:13:22 +0200

    test image load script

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-from multiprocessing import Pool
+import numpy as np
+import os
+from glob import glob
 import cv2
 
 
-def test(data):
-    cv2.CALIB_FIX_K1 = 1
-    print(data)
-    return True
+def sort(element):
+    return int(element.split('.')[0].split('_')[-1])
 
 
-with Pool(5) as p:
-    res = p.map(test, [1, 2, 3])
+camera_ids = [0, 1]
+
+for camera_id in camera_ids:
+    files = glob(os.path.join(
+        'images', f'camera_{camera_id}', '*'))
+    files.sort(key=sort)
+    for fname in files:
+        image = np.load(fname)
+        cv2.imshow('Camera View', image)
+
+        if cv2.waitKey(0) == ord('q'):
+            continue
