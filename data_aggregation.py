f069d949a4b880c266dbb8e40ceacef764368a3d
athos <frederik.polachowski@tu-dortmund.de>
2021-04-23 16:13:07 +0200

    changed ids

--- data_aggregation.py
+++ data_aggregation.py
@@ -87,8 +87,8 @@ class Processor():
 
 if __name__ == "__main__":
     # the selected cameras out of [0, 1, 2, 3, 4, 5, 6, 7]
-    camera_ids = [0, 1, ]
-    object_ids = ['KLT_8_neu']
+    camera_ids = [0, 1, 2, 3, 4, 5, 6, 7]
+    object_ids = ['KLT_34_neu']
 
     processor = Processor(camera_ids, object_ids)
     processor.record()
