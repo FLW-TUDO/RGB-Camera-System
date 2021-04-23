d8947ca07e796cdaa4b5e492d5b6d5a0e2499c9c
athos <frederik.polachowski@tu-dortmund.de>
2021-04-23 13:18:57 +0200

    fixed errors in script, moved the save method to the camera

--- data_aggregation.py
+++ data_aggregation.py
@@ -1,92 +1,94 @@
 from camera import Camera
 from glob import glob
+from itertools import product
 from vicon_tracker import ObjectTracker
 import numpy as np
-import os, csv, cv2
+import os
+import csv
+import sys
+import cv2
+from multiprocessing import Pool
+from time import time
+from copy import deepcopy
+
 
 class Processor():
-    def __init__(self, cameras, objects):
-        Thread.__init__(self)
-        self.cameras = cameras
+    def __init__(self, camera_ids, objects):
+        self.camera_ids = camera_ids
         self.objects = objects
-        self.resolution = (int(2592), int(2048))
-
-        self.createFolder()
-        self.image_index = self.getIndex()
 
         self.tracker = ObjectTracker()
         self.tracker.connect()
 
-    def getIndex(self):
+    def getIndex(self, cameras):
         index = 0
-        camera = self.cameras[0]
+        camera = cameras[0]
         if os.path.exists(os.path.join('images', f'camera_{camera.id}')):
-            images = glob(os.path.join('images', f'camera_{camera.id}'))
-            index = int(images[-1].split('.')[0].split('_')[-1])
+            files = glob(os.path.join('images', f'camera_{camera.id}', '*'))
+            for fname in files:
+                findex = int(fname.split('.')[0].split('_')[-1])
+                index = findex + 1 if findex >= index else index
         return index
 
-    def createFolder(self):
-        for camera in self.cameras:
+    def createFolder(self, cameras):
+        for camera in cameras:
             if not os.path.exists(os.path.join('images', f'camera_{camera.id}')):
-                os.makedirs(os.path('images', f'camera_{camera.id}'))
+                os.makedirs(os.path.join('images', f'camera_{camera.id}'))
 
-    def aquire_object_positions(self):
+    def aquire_object_positions(self, image_index):
         with open('data.csv', 'a') as f:
             writer = csv.writer(f)
             for object_id in self.objects:
-                positions = self.tracker.aquire_Object_MarkerPositions(object_id)
-                writer.writerow([object_id,f'image_{self.image_index}', positions])
-
+                positions = self.tracker.aquire_Object_Trans(
+                    object_id)
+                rotation = self.tracker.aquire_Object_RotQuaternion(object_id)
+                writer.writerow(
+                    [object_id, f'image_{image_index}', positions, rotation])
 
     # retrieves image from camera and preprocesses image to store it
-    def getImage(self, camera):
-        image = camera.getImage()
-        if image is None:
-            print('Camera {} is not running'.format(camera.id))
-            return False
-        
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        image = cv2.resize(image, self.resolution)
-        image = cv2.flip(image, 0)
-        image = cv2.flip(image, 1)
 
-        np.savez(os.path.join('images', f'camera_{camera.id}', f'image_{self.image_index}.npz'), image)
-
-        return True
+    def getImage(self, camera, index):
+        res = camera.saveImage(index)
+        return res
 
     def stop(self):
         self.running = False
 
     def record(self):
         self.running = True
+
+        cameras = []
+        for cam_id in self.camera_ids:
+            cameras.append(Camera(cam_id, size_factor=4))
+            print('Started Camera {}'.format(cam_id))
+
+        self.createFolder(cameras)
+        image_index = self.getIndex(cameras)
+
         print('Recording Cameras {}. Press q to exit.'.format(
-            [i for i in range(len(self.cameras))]))
+            [i for i in range(len(cameras))]))
 
         while self.running:
-            with Pool(len(self.cameras)) as p:
-                res = p.map(self.getImage, self.cameras)
+            time_start = time()
+            for camera in cameras:
+                res = self.getImage(camera, image_index)
 
-            self.aquire_object_positions()
-            self.image_index += 1
+            self.aquire_object_positions(image_index)
+            image_index += 1
 
             # Displays FPS
-            sys.stdout.write('\rFramerates: {}'.format(
-                [round(camera.fps, 2) for camera in self.cameras]))
+            sys.stdout.write('\rFramerates: {}; Recorded fps: {}'.format(
+                [round(camera.fps, 2) for camera in cameras], 1/(time()-time_start)))
             sys.stdout.flush()
 
-            if cv2.waitKey(50) == ord('q'):
+            if cv2.waitKey(100) == ord('q'):
                 break
+
 
 if __name__ == "__main__":
     # the selected cameras out of [0, 1, 2, 3, 4, 5, 6, 7]
-    camera_ids = [0, 1, 2, 3, 4, 5, 6]
-    object_ids = ['test_object']
+    camera_ids = [0, 1, ]
+    object_ids = ['KLT_8_neu']
 
-    cameras = []
-    for cam_id in camera_ids:
-        cameras.append(Camera(cam_id))
-        print('Started Camera {}'.format(cam_id))
-
-    processor = Processor(cameras)
+    processor = Processor(camera_ids, object_ids)
     processor.record()
-    
