ac0c4a9bd53a6975c874a38422de65bd9d3e124b
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2021-08-16 13:39:43 +0200

    changed formatting

--- transformations.py
+++ transformations.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from vicon_tracker import ObjectTracker
 from pyquaternion import Quaternion
@@ -11,19 +10,24 @@ tracker.connect()
 
 def get_obj_to_cam_transform(obj_name, cam_id):
     obj_to_world_trans = tracker.aquire_Object_Trans(obj_name)
-    obj_to_world_rot_quat = tracker.aquire_Object_RotQuaternion(
-        obj_name)  # x,y,z,w
+    obj_to_world_rot_quat = tracker.aquire_Object_RotQuaternion(obj_name)  # x,y,z,w
 
     obj_to_world_rot_quat_2 = Quaternion(
-        obj_to_world_rot_quat[3], obj_to_world_rot_quat[0], obj_to_world_rot_quat[1], obj_to_world_rot_quat[2])  # w,x,y,z
+        obj_to_world_rot_quat[3],
+        obj_to_world_rot_quat[0],
+        obj_to_world_rot_quat[1],
+        obj_to_world_rot_quat[2],
+    )  # w,x,y,z
     obj_to_world_rot = obj_to_world_rot_quat_2.rotation_matrix
 
-    with open('cam_poses.json', 'r') as f:
+    with open("cam_poses.json", "r") as f:
         data = json.load(f)
-    cam_to_world_trans = data['cam'+str(cam_id)]['trans']
-    cam_to_world_rot = data['cam'+str(cam_id)]['rot']
-    obj_to_cam_trans = [(cam_to_world_trans[i] - obj_to_world_trans[i])
-                        for i in range(len(obj_to_world_trans))]
+    cam_to_world_trans = data["cam" + str(cam_id)]["trans"]
+    cam_to_world_rot = data["cam" + str(cam_id)]["rot"]
+    obj_to_cam_trans = [
+        (cam_to_world_trans[i] - obj_to_world_trans[i])
+        for i in range(len(obj_to_world_trans))
+    ]
     obj_to_cam_rot = obj_to_world_rot * np.transpose(cam_to_world_rot)
     return (obj_to_cam_trans, obj_to_cam_rot)
 
@@ -34,6 +38,6 @@ def get_obj_to_cam_transform(obj_name, cam_id):
 
 
 if __name__ == "__main__":
-    obj_trans, obj_rot = get_obj_to_cam_transform('KLT_8_neu', 0)
+    obj_trans, obj_rot = get_obj_to_cam_transform("KLT_8_neu", 0)
     print(obj_trans)
     print(obj_rot)
