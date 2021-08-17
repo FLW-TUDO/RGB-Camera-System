e929bd1fdbc4827620fe90bd0c66bd015dce527e
Hazem Youssef <hazem.youssef@tu-dortmund.de>
2021-08-17 21:24:56 +0200

    minor changes

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rgb_camera_system',
+    version='0.1.0',
+    packages=find_packages(include=['rgb_camera_system', 'rgb_camera_system.*'])
+)
