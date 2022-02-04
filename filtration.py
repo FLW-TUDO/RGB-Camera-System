f26f37b9785b6c2d6ff5cb5d535b893df2e01021
Hazem Youssef <hazem.youssef@tu-dortmund.de>
2022-02-04 21:00:55 +0100

    added filtration of invalid images

---
+++ filtration.py
@@ -0,0 +1,51 @@
+import os
+import csv
+import shutil
+import cv2
+
+'''
+    Simple script for manually filtering out invalid images
+'''
+
+cams = [0, 1, 2, 3, 4, 5]
+recording = '18_46 01_02_2022'
+
+def filter_images():
+    corrupt_imgs = 0
+    for cam in cams:
+        recording_path = os.path.join('./recordings', recording, f'camera_{cam}', 'images')
+        filtered_path = os.path.join('./recordings', recording + '_filtered', f'camera_{cam}', 'images')
+        if not os.path.exists(filtered_path):
+            os.makedirs(filtered_path)
+        csv_gt = os.path.join(recording_path, 'data.csv')
+        csv_filtered = os.path.join(filtered_path, 'data.csv')
+        with open(csv_gt) as in_file, open(csv_filtered, 'w', newline='') as out_file:
+            reader = csv.reader(in_file)
+            reader_list = list(reader)
+            writer = csv.writer(out_file)
+            header = reader_list[0]
+            writer.writerow(header)
+            for row in reader_list[1:]:
+                img_path = row[header.index('ImageName')]
+                obj_id = row[header.index('ObjectID')]
+                img = cv2.imread(img_path)
+                if img is not None:
+                    cv2.imshow(f'Object {obj_id} in Camera {cam}?', img)
+                    key = cv2.waitKey(0)
+                    if key == 97: #a
+                        print('Image Accepted')
+                        shutil.copy2(img_path, filtered_path)
+                        writer.writerow(row)
+                    elif key == 114: #r
+                        print('Image Rejected!')
+                    elif key == 113: #q
+                        cv2.destroyAllWindows()
+                        break
+                    cv2.destroyAllWindows()
+                else:
+                    corrupt_imgs += 1
+                    continue
+
+
+if __name__ == '__main__':
+    filter_images()
