122522d6e549ee11bbd86531264bd18db469aae2
Hazem Youssef <hazem.youssef@tu-dortmund.de>
2022-02-16 23:36:46 +0100

    removed obtainment of object poses in vicon

--- DataCollector.py
+++ DataCollector.py
@@ -5,6 +5,7 @@ from PyVicon.vicon_tracker import ObjectTracker
 from threading import Thread
 import os, csv, cv2
 from datetime import datetime
+from calibration.obj_gt_pose import get_obj_gt_transform
 
 
 obj_ids = {"KLT_8_neu": 1,
@@ -33,24 +34,21 @@ class Processor(Thread):
 
         os.makedirs(os.path.join(self.folder_path, self.name, "images"))
 
-        self.vicon = ObjectTracker()
-        self.vicon.connect()
         self.camera = Camera(index)
-
         self.start()
 
     def run(self):
         """
-        Gets poses for all objects of interest that could (possibly) be in an image.
+        Gets poses for all objects of interest that could (possibly) be within the camera view.
         The annotated output would contain as many images with the same ID (and different obj id) as there are objects
-        of interest.
+        of interest. Images should be filtered at a later stage
         """
         while self.running:
             data = {"image": self.camera.getImage()}
             for obj in list(obj_ids.keys()):
                 #data['objects']
-                obj_trans = self.vicon.aquire_Object_Trans(object_name=obj)
-                obj_rot = self.vicon.aquire_Object_RotQuaternion(object_name=obj)
+                obj_trans = get_obj_gt_transform(self.camera, obj)[0]
+                obj_rot = get_obj_gt_transform(self.camera, obj)[1]
                 pose = {'obj_trans': obj_trans, 'obj_rot': obj_rot}
                 pose_copy = copy.deepcopy(pose)
                 data[obj] = pose_copy
@@ -74,7 +72,7 @@ class Processor(Thread):
     """
 
     def writeData(self, data, titles):
-        path = os.path.join(self.folder_path, self.name, "images")
+        path = os.path.join(self.folder_path, self.name)
         with open(
             os.path.join(path, "data.csv"), "a", newline=""
         ) as csvfile:
@@ -89,7 +87,7 @@ class Processor(Thread):
                         obj_keys.append(key)
                 for obj in obj_keys:
                     img_path = os.path.join(path, str(self.imageIndex))
-                    print(f'Obj ID: {obj}, Img path: {img_path}, Trans: {data[obj]["obj_trans"]}, Rot: {data[obj]["obj_rot"]}')
+                    #print(f'Obj ID: {obj}, Img path: {img_path}, Trans: {data[obj]["obj_trans"]}, Rot: {data[obj]["obj_rot"]}')
                     writer.writerow([obj_ids[obj], img_path+'.png', data[obj]['obj_trans'], data[obj]['obj_rot']])
         cv2.imwrite(os.path.join(path, f"{self.imageIndex}.png"), data["image"])
 
@@ -99,7 +97,7 @@ if __name__ == "__main__":
     capture_time = 2000 #ms
     while True:
         for processor in processors:
-            cv2.imshow(f'cam_{processor.name}', processor.camera.getImage())
+            #cv2.imshow(f'cam_{processor.name}', processor.camera.getImage())
             key = cv2.waitKey(capture_time)
             if key == 27:
                 break
---
+++ calibration/obj_gt_pose.py
@@ -0,0 +1,7 @@
+import numpy as np
+from camera_localization impo
+
+
+
+def get_obj_gt_pose():
+    pass
