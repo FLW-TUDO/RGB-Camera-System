72949914940385c2977b1141d15838f16289d13a
Hazem Youssef <hazem.youssef@tu-dortmund.de>
2022-02-18 20:29:24 +0100

    improve paths handling

--- DataCollector.py
+++ DataCollector.py
@@ -1,5 +1,4 @@
 import copy
-from threading import Thread
 from Camera.CVBCamera import Camera
 from PyVicon.vicon_tracker import ObjectTracker
 from threading import Thread
@@ -12,7 +11,7 @@ obj_ids = {"KLT_8_neu": 1,
            "KLT_27_neu": 2,
            "KLT_32_neu": 3
            }
-cameras = [0, 1, 2, 3, 4, 5]  # 0,1,2,3,4,5,6,7
+cameras = [0, 1]  # 0,1,2,3,4,5,6,7
 path = "recordings"
 
 
@@ -32,8 +31,6 @@ class Processor(Thread):
         now = datetime.now().strftime("%H_%M %d_%m_%Y") #adding seconds results in multiple directories creation
         self.folder_path = os.path.join(path, now)
 
-        os.makedirs(os.path.join(self.folder_path, self.name, "images"))
-
         self.camera = Camera(index)
         self.start()
 
@@ -41,7 +38,14 @@ class Processor(Thread):
         """
         Gets poses for all objects of interest that could (possibly) be within the camera view.
         The annotated output would contain as many images with the same ID (and different obj id) as there are objects
-        of interest. Images should be filtered at a later stage
+        of interest. Images should be filtered at a later stage.
+
+        Writes data to an intermediary dict
+
+        data: {
+            "image": numpy.array (RGB image)
+            "object_0001": dict | object translation and rotation
+            }
         """
         while self.running:
             data = {"image": self.camera.getImage()}
@@ -55,46 +59,43 @@ class Processor(Thread):
                 #print(data[obj])
 
             if self.imageIndex == 0:
-                self.writeData(data, titles=True)
-            self.writeData(data, titles=False)
+                self.writeData(data, header=True)
+            self.writeData(data, header=False)
             self.imageIndex += 1
             data.clear()
 
     def stop(self):
         self.running = False
 
-    """
-    data: {
-        "image": numpy.array (RGB image)
-        "object_0001": dict | object translation and rotation
-    }
-    Writes as csv line and an image file for each object present in the scene
-    """
-
-    def writeData(self, data, titles):
-        path = os.path.join(self.folder_path, self.name)
-        with open(
-            os.path.join(path, "data.csv"), "a", newline=""
-        ) as csvfile:
+    def writeData(self, data, header):
+        """
+        Writes as csv line and an image file for each object present in the scene
+        """
+        cam_path = os.path.join(self.folder_path, self.name)
+        images_path = os.path.join(cam_path, 'images')
+        if not os.path.exists(images_path):
+            os.makedirs(images_path)
+        with open(os.path.join(cam_path, "data.csv"), "a", newline="") as csvfile:
             writer = csv.writer(csvfile)
-            if titles:
+            if header:
                 writer.writerow(['ObjectID', 'ImageName', 'camToObjTrans', 'camToObjRot', 'symmetry'])
             else:
                 #print(data.keys())
-                obj_keys = []
-                for key in list(data.keys()):
-                    if key is not 'image':
-                        obj_keys.append(key)
-                for obj in obj_keys:
-                    img_path = os.path.join(path, str(self.imageIndex))
+                # obj_keys = []
+                # for key in list(data.keys()):
+                #     if key is not 'image': # get object IDs
+                #         obj_keys.append(key)
+                for obj in list(obj_ids.keys()):
+                    img_path = os.path.join(images_path, str(self.imageIndex) + '.png')
                     #print(f'Obj ID: {obj}, Img path: {img_path}, Trans: {data[obj]["obj_trans"]}, Rot: {data[obj]["obj_rot"]}')
-                    writer.writerow([obj_ids[obj], img_path+'.png', data[obj]['obj_trans'], data[obj]['obj_rot']])
-        cv2.imwrite(os.path.join(path, f"{self.imageIndex}.png"), data["image"])
+                    writer.writerow([obj_ids[obj], img_path, data[obj]['obj_trans'], data[obj]['obj_rot']])
+                cv2.imwrite(img_path, data["image"])
 
 
 if __name__ == "__main__":
     processors = [Processor(index)for index in cameras]
     capture_time = 2000 #ms
+    #TODO: replace loop with button snapping
     while True:
         for processor in processors:
             #cv2.imshow(f'cam_{processor.name}', processor.camera.getImage())
---
+++ calibration/camera_localization_2.py
@@ -0,0 +1,184 @@
+import csv
+from calibration.calibration_all import get_intrinsics, get_extrinsics
+from icecream import ic
+import numpy as np
+import ast
+from scipy.spatial.transform import Rotation as R
+import cv2
+import glob
+import os
+import csv
+
+
+'''
+The script finds the location of a given camera in the vicon frame, assuming a vicon-tracked chessboard is used.
+'''
+
+vicon_chessboard_pose_csv_file = '../vicon_pose_chessboard.csv'
+images = '../images/snapper/cam_localization/*.png'
+cam_locations_csv_file = '../cam_locations.csv'
+
+scale = 130
+a = 7
+b = 5
+
+# ret, mtx, dist, newcameramtx, roi, _, _ = get_intrinsics(
+#     images, a, b, scale, visulaize=False)
+
+newcameramtx = np.array([[784.04003906, 0.0, 624.41500984],
+                [0.0, 783.08575439, 521.11914666],
+                [0.0, 0.0, 1.0]])
+dist = np.array([[-0.18619846, 0.14989978, 0.00036384, -0.00132873, -0.04512847]])
+
+def get_homogenous_form(rot, trans):
+    '''
+    @param rot: rotation matrix
+    @type rot: numpy array
+    @param trans: translation vector
+    @type trans: numpy
+    @return: homogenous form
+    @rtype: numpy array
+    '''
+    mat = np.column_stack((rot, trans))
+    mat_homog = np.row_stack((mat, [0.0, 0.0, 0.0, 1.0]))
+    return mat_homog
+
+
+def get_chessboard2vicon_transform(csv_file, img_path):
+    with open(csv_file) as f:
+        reader = csv.reader(f)
+        reader_list = list(reader)
+        path_mod = os.path.split(img_path)[-1]
+        img_num = path_mod.split('.')[-2]
+        ic(img_num)
+
+        search_entry = 'img_' + img_num
+        ic(search_entry)
+        for i, entries in enumerate(reader_list):
+            for j in entries:
+                if j == search_entry:
+                    index = i
+                    ic(index)
+        row = reader_list[int(index)]
+
+        chessboard2vicon_trans = np.array(
+            ast.literal_eval(row[1]))  # converts string list
+        ic(chessboard2vicon_trans)
+
+        chessboard2vicon_rot_quat = ast.literal_eval(row[2])
+        ic(chessboard2vicon_rot_quat)
+        chessboard2vicon_rot_quat = R.from_quat(chessboard2vicon_rot_quat)
+        ic(chessboard2vicon_rot_quat)
+        chessboard2vicon_rot = R.as_matrix(
+            chessboard2vicon_rot_quat)  # rotation matrix form
+        ic(chessboard2vicon_rot)
+
+        chessboard2vicon_transform = get_homogenous_form(
+            chessboard2vicon_rot, chessboard2vicon_trans)
+        ic(chessboard2vicon_transform)
+
+    # return chessboard2vicon_trans, chessboard2vicon_rot
+    return chessboard2vicon_transform
+
+
+def get_origin_chess2cam_transform(img_path):
+    ret, rvecs, tvecs = get_extrinsics(img_path, a, b, scale, newcameramtx, dist)
+    ic(tvecs)
+    origin_chess2cam_trans = np.array(tvecs.reshape(1, 3)[0])
+    ic(origin_chess2cam_trans)
+
+    origin_chess2cam_rot_axis_angle = rvecs.reshape(
+        1, 3)[0]  # given as a rotation vector (axis-angle)
+    ic(origin_chess2cam_rot_axis_angle)
+    origin_chess2cam_rot, _ = cv2.Rodrigues(
+        origin_chess2cam_rot_axis_angle)  # rotation matrix form
+    ic(origin_chess2cam_rot)
+
+    chessboard2vicon_transform = get_homogenous_form(
+        origin_chess2cam_rot, origin_chess2cam_trans)
+    ic(chessboard2vicon_transform)
+
+    # return origin_chess2cam_trans, origin_chess2cam_rot
+    return chessboard2vicon_transform
+
+
+def get_origin_chess2chessboard_transform(scale):
+    '''
+        Offset correction from location of marker on chessboard to first corner location (used in extrinsic calculation)
+        Origin_chess is located at the first intersection
+    '''
+    origin_chess2chessboard_trans = np.array([scale, scale, 0])
+    ic(origin_chess2chessboard_trans)
+
+    origin_chess2chessboard_rot = np.eye(3)
+    ic(origin_chess2chessboard_rot)
+
+    origin_chess2chessboard_transform = get_homogenous_form(
+        origin_chess2chessboard_rot, origin_chess2chessboard_trans)
+    ic(origin_chess2chessboard_transform)
+
+    # return origin_chess2chessboard_trans, origin_chess2chessboard_rot
+    return origin_chess2chessboard_transform
+
+def get_cam2vicon_transform(img_path):
+    chessboard2vicon_transform = get_chessboard2vicon_transform(vicon_chessboard_pose_csv_file, img_path)
+    origin_chess2cam_transform = get_origin_chess2cam_transform(img_path)
+    origin_chess2chessboard_transform = get_origin_chess2chessboard_transform(scale)
+
+    origin_chess2vicon_transform = chessboard2vicon_transform.dot(origin_chess2chessboard_transform)
+    cam2vicon_transform = origin_chess2vicon_transform.dot(np.linalg.inv(origin_chess2cam_transform))
+    ic(cam2vicon_transform)
+    cam2vicon_trans = cam2vicon_transform[0:3, 3]
+    cam2vicon_rot = cam2vicon_transform[0:3, 0:3]
+
+    # cam2vicon_rot_mat = R.from_matrix(cam2vicon_transform[0:3, 0:3])
+    # cam2vicon_rot_euler = np.degrees(cam2vicon_rot_mat.as_euler('xyz', degrees=False))
+    return cam2vicon_trans, cam2vicon_rot #as a rotation matrix
+
+#TODO: retrieve intrinsics according to cam id
+def get_cam_location(images_path, cam_id=0):
+    images = glob.glob(images_path)
+    cam2vicon_trans_list = np.array([0, 0, 0])
+    cam2vicon_rot_list = np.array([0, 0, 0])
+    for img in images:
+        trans, rot = get_cam2vicon_transform(img)
+        cam2vicon_trans_list = np.add(cam2vicon_trans_list, trans)
+        cam2vicon_rot_mat = R.from_matrix(rot)
+        cam2vicon_rot_euler = np.degrees(cam2vicon_rot_mat.as_euler('xyz', degrees=False))
+        cam2vicon_rot_list = np.add(cam2vicon_rot_list, cam2vicon_rot_euler)
+        ic(cam2vicon_trans_list)
+        ic(cam2vicon_rot_list)
+    cam2vicon_trans_avg = np.divide(cam2vicon_trans_list, len(images))
+    ic(cam2vicon_trans_avg)
+    # TODO: convert to quaternion to get average - slerp
+    cam2vicon_rot_avg = np.divide(cam2vicon_rot_list, len(images))
+    ic(cam2vicon_rot_avg)
+    cam2vicon_rot_avg = R.from_euler('xyz', cam2vicon_rot_avg, degrees=True)
+    cam2vicon_rot_avg_mat = cam2vicon_rot_avg.as_matrix()
+    ic(cam2vicon_rot_avg_mat)
+    save_cam_location(cam_locations_csv_file, cam_id, cam2vicon_trans_avg.tolist(), cam2vicon_rot_avg_mat.tolist())
+    return cam2vicon_trans_avg, cam2vicon_rot_avg_mat
+
+#TODO: add header
+def save_cam_location(csv_file, cam_id, cam_trans, cam_rot):
+    with open(csv_file, 'a', newline='') as out_file:
+        writer = csv.writer(out_file)
+        #header = ['cam_id', 'cam_trans', 'cam_rot']
+        #writer.writerow(header)
+        data = [cam_id, cam_trans, cam_rot]
+        writer.writerow(data)
+
+def invert_homog_transfrom(homog_trans):
+    trans = homog_trans[0:3, 3]
+    rot = homog_trans[0:3, 0:3]
+    rot_inv = np.linalg.inv(rot)
+    homog_inv = get_homogenous_form(rot_inv, -1*(rot_inv.dot(trans)))
+    ic(homog_inv)
+    return homog_inv
+
+
+if __name__ == '__main__':
+    trans, rot = get_cam_location(images)
+    ic(trans)
+    ic(rot)
+
