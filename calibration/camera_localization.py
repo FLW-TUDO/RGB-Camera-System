026d6f8eb6e8a140117034a13d5286468f5fbe59
Hazem Youssef <hazem.youssef@tu-dortmund.de>
2022-02-21 23:01:39 +0100

    integrated fetching calibration parameters with cam id

--- calibration/camera_localization.py
+++ calibration/camera_localization.py
@@ -11,30 +11,32 @@ import csv
 
 
 '''
-The script finds the location of a given camera in the vicon frame, assuming a vicon-tracked chessboard is used.
+The script finds the location of a given camera in the vicon frame using snapped images of a vicon-tacked checkerboard.
 '''
 
 vicon_chessboard_pose_csv_file = '../vicon_pose_chessboard.csv' #intermediary csv file
 images = '../images/snapper/cam_localization/*.png'
 images_undist = '../images/snapper/cam_localization/undistorted'
 cam_locations_csv_file = '../cam_locations.csv' #final csv file
+calib_params_csv_file = '../calib_params_all.csv'
 
 scale = 130
 a = 7
 b = 5
 
-# ret, mtx, dist, newcameramtx, roi, _, _ = get_intrinsics(
-#     images, a, b, scale, visulaize=False)
+if not os.path.exists(images_undist):
+    os.makedirs(images_undist)
 
-
-mtx = np.array([[870.04652949,   0.0, 644.25313422],
-                [0.0, 870.04874977, 543.69294777],
-                [0.0,   0.0,   1.0]])
-newcameramtx = np.array([[808.68377686,   0.0, 643.57414451],
-                        [0.0, 810.70056152, 547.12750201],
-                        [0.0,   0.0,   1.0]])
-dist = np.array([[-0.164559885,  0.108215606,  0.00257066836, -0.000120995529, -0.00928914594]])
-roi = (5, 10, 1285, 1003)
+def get_calib_params(csv_file, cam_id):
+    with open(csv_file) as f:
+        reader = csv.reader(f)
+        for row in list(reader):
+            if row[0] == str(cam_id):
+                mtx = np.array(ast.literal_eval(row[1]))
+                newcameramtx = np.array(ast.literal_eval(row[2]))
+                dist = np.array(ast.literal_eval(row[3]))
+                roi = np.array(ast.literal_eval(row[4]))
+                return mtx, newcameramtx, dist, roi
 
 def get_homogenous_form(rot, trans):
     '''
@@ -50,7 +52,7 @@ def get_homogenous_form(rot, trans):
     return mat_homog
 
 
-def get_chessboard2vicon_transform(csv_file, img_path):
+def get_chessboard2vicon_transform(csv_file, img_path, cam_id):
     with open(csv_file) as f:
         reader = csv.reader(f)
         reader_list = list(reader)
@@ -58,33 +60,38 @@ def get_chessboard2vicon_transform(csv_file, img_path):
         img_num = path_mod.split('.')[-2]
         ic(img_num)
 
-        search_entry = 'img_' + img_num
-        ic(search_entry)
-        for i, entries in enumerate(reader_list):
-            for j in entries:
-                if j == search_entry:
-                    index = i
-                    ic(index)
-        row = reader_list[int(index)]
+        img_search_entry = 'img_' + img_num
+        cam_search_entry = f'cam_{cam_id}'
+        ic(img_search_entry)
+        ic(cam_search_entry)
+        # for i, entries in enumerate(reader_list):
+        #     for j in entries:
+        #         if j == img_search_entry:
+        #             index = i
+        #             ic(index)
+        # row = reader_list[int(index)]
 
-        chessboard2vicon_trans = np.array(ast.literal_eval(row[1]))  # converts string list
-        ic(chessboard2vicon_trans)
+        for row in reader_list:
+            ic(row)
+            if row[0] == cam_search_entry and row[1] == img_search_entry:
+                chessboard2vicon_trans = np.array(ast.literal_eval(row[2]))  # converts string list
+                ic(chessboard2vicon_trans)
 
-        chessboard2vicon_rot_quat = ast.literal_eval(row[2])
-        ic(chessboard2vicon_rot_quat)
-        chessboard2vicon_rot_quat = R.from_quat(chessboard2vicon_rot_quat)
-        ic(chessboard2vicon_rot_quat)
-        chessboard2vicon_rot = R.as_matrix(chessboard2vicon_rot_quat)  # rotation matrix form
-        ic(chessboard2vicon_rot)
+                chessboard2vicon_rot_quat = ast.literal_eval(row[3])
+                ic(chessboard2vicon_rot_quat)
+                chessboard2vicon_rot_quat = R.from_quat(chessboard2vicon_rot_quat)
+                ic(chessboard2vicon_rot_quat)
+                chessboard2vicon_rot = R.as_matrix(chessboard2vicon_rot_quat)  # rotation matrix form
+                ic(chessboard2vicon_rot)
 
-        chessboard2vicon_transform = get_homogenous_form(chessboard2vicon_rot, chessboard2vicon_trans)
-        ic(chessboard2vicon_transform)
+                chessboard2vicon_transform = get_homogenous_form(chessboard2vicon_rot, chessboard2vicon_trans)
+                ic(chessboard2vicon_transform)
 
-    # return chessboard2vicon_trans, chessboard2vicon_rot
-    return chessboard2vicon_transform
+                return chessboard2vicon_transform
 
 
-def get_origin_chess2cam_transform(img_path):
+def get_origin_chess2cam_transform(img_path, cam_id):
+    _, newcameramtx, dist, _ = get_calib_params(calib_params_csv_file, cam_id)
     ret, rvecs, tvecs = get_extrinsics(img_path, a, b, scale, newcameramtx, dist)
     ic(tvecs)
     origin_chess2cam_trans = np.array(tvecs.reshape(1, 3)[0])
@@ -95,11 +102,11 @@ def get_origin_chess2cam_transform(img_path):
     origin_chess2cam_rot, _ = cv2.Rodrigues(origin_chess2cam_rot_axis_angle)  # rotation matrix form
     ic(origin_chess2cam_rot)
 
-    chessboard2vicon_transform = get_homogenous_form(origin_chess2cam_rot, origin_chess2cam_trans)
-    ic(chessboard2vicon_transform)
+    origin_chess2cam_transform = get_homogenous_form(origin_chess2cam_rot, origin_chess2cam_trans)
+    ic(origin_chess2cam_transform)
 
     # return origin_chess2cam_trans, origin_chess2cam_rot
-    return chessboard2vicon_transform
+    return origin_chess2cam_transform
 
 
 def get_origin_chess2chessboard_transform(scale):
@@ -119,9 +126,9 @@ def get_origin_chess2chessboard_transform(scale):
     # return origin_chess2chessboard_trans, origin_chess2chessboard_rot
     return origin_chess2chessboard_transform
 
-def get_cam2vicon_transform(img_path):
-    chessboard2vicon_transform = get_chessboard2vicon_transform(vicon_chessboard_pose_csv_file, img_path)
-    origin_chess2cam_transform = get_origin_chess2cam_transform(img_path)
+def get_cam2vicon_transform(img_path, cam_id):
+    chessboard2vicon_transform = get_chessboard2vicon_transform(vicon_chessboard_pose_csv_file, img_path, cam_id)
+    origin_chess2cam_transform = get_origin_chess2cam_transform(img_path, cam_id)
     origin_chess2chessboard_transform = get_origin_chess2chessboard_transform(scale)
 
     origin_chess2vicon_transform = chessboard2vicon_transform.dot(origin_chess2chessboard_transform)
@@ -141,9 +148,10 @@ def get_cam_location(images_path, cam_id, use_undist=True):
     cam2vicon_rot_list = np.array([0, 0, 0])
     for img_path in images:
         if use_undist:
+            mtx, newcameramtx, dist, roi = get_calib_params(calib_params_csv_file, cam_id)
             img_path = undistort_and_save(img_path, mtx, dist, newcameramtx, roi, visualize=True, save_path=images_undist)
             ic(img_path)
-        trans, rot = get_cam2vicon_transform(img_path)
+        trans, rot = get_cam2vicon_transform(img_path, cam_id)
         cam2vicon_trans_list = np.add(cam2vicon_trans_list, trans)
         cam2vicon_rot_mat = R.from_matrix(rot)
         cam2vicon_rot_euler = np.degrees(cam2vicon_rot_mat.as_euler('XYZ', degrees=False))
@@ -163,6 +171,7 @@ def get_cam_location(images_path, cam_id, use_undist=True):
 
 #TODO: add header, check if cam id already exists
 def save_cam_location(csv_file, cam_id, cam_trans, cam_rot):
+    #TODO: check if nan
     with open(csv_file, 'a', newline='') as out_file:
         writer = csv.writer(out_file)
         #header = ['cam_id', 'cam_trans', 'cam_rot']
@@ -180,7 +189,7 @@ def invert_homog_transfrom(homog_trans):
 
 
 if __name__ == '__main__':
-    trans, rot = get_cam_location(images, use_undist=True)
+    trans, rot = get_cam_location(images, cam_id=1, use_undist=True)
     # ic(trans)
     # ic(rot)
     # trans, rot = get_chessboard2vicon_transform(vicon_chessboard_pose_csv_file)
